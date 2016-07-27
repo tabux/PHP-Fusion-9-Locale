a62b26c470e2481f2024b3ebce19c60e577236cc
Tomasz "jantom" Jankowski <jantom@jantom.info>
2016-07-27 12:09:58 +0200

    Przetłumaczone aktualności News translated

--- Polish/infusions/news/locale/Polish/news_cats.php
+++ Polish/infusions/news/locale/Polish/news_cats.php
@@ -1,9 +1,10 @@
 <?php
-$locale['400'] = "News Categories";
-$locale['401'] = "Category:";
-$locale['402'] = "Number of Items:";
-$locale['403'] = "None-Categorised News";
-$locale['404'] = "There are no news items in this category.";
-$locale['405'] = "More";
-$locale['406'] = "Show All Categories";
-$locale['407'] = "No News Categories defined";
+$locale['400'] = "Kategorie aktualności";
+$locale['401'] = "Kategoria:";
+$locale['402'] = "Liczba pozycji:";
+$locale['403'] = "Bez kategorii";
+$locale['404'] = "Brak udostępnionych elementów w&nbsp;kategorii.";
+$locale['405'] = "Więcej";
+$locale['406'] = "Pokaż wszystkie kategorie";
+$locale['407'] = "Brak udostępnionych kategorii aktualności.";
+?>
