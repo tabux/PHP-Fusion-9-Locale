53657b33eedd2dae210c8d2bead1e6cdf62666f2
tomeeek64 <mediauser64@hotmail.com>
2016-07-30 21:58:35 +0200

    Update user_web.php

--- Polish/locale/Polish/user_fields/user_web.php
+++ Polish/locale/Polish/user_fields/user_web.php
@@ -1,6 +1,6 @@
 <?php
-$locale['uf_web'] = "Website";
-$locale['uf_web_desc'] = "URL website";
-$locale['uf_web_error'] = "Website can not be left empty.";
-$locale['uf_web_001'] = "Visit";
+$locale['uf_web'] = "Strona internetowa";
+$locale['uf_web_desc'] = "Adres URL strony internetowej";
+$locale['uf_web_error'] = "Strona internetowa nie może być pozostawiona pusta.";
+$locale['uf_web_001'] = "Odwiedź";
 
