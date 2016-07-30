779e36feef4b7d6b6e1f3994c16c15bcfdd7b4d1
Tomasz "jantom" Jankowski <jantom@jantom.info>
2016-07-30 18:38:41 +0200

    Blog translated Przetłumaczono blogi

--- Polish/infusions/blog/locale/Polish/blog.php
+++ Polish/infusions/blog/locale/Polish/blog.php
@@ -1,23 +1,23 @@
 <?php
 
 $locale['blog_1000'] = "Blog";
-$locale['blog_1001'] = "Blog Home";
-$locale['blog_1003'] = "Blog Category";
-$locale['blog_1004'] = "Blog Archive";
-$locale['blog_1005'] = "Blog Author";
-$locale['blog_1006'] = "Read more";
-$locale['blog_2001'] = "Most Recent Blogs";
-$locale['blog_2002'] = "Most Commented Blog";
-$locale['blog_2003'] = "Highest Rating Blog";
-$locale['blog_3000'] = "No Blogs has been posted yet";
-$locale['blog_3001'] = "No Category";
-$locale['blog_3002'] = "No Post";
-$locale['blog_3003'] = "No Author";
-$locale['testimonial_rank'] = "I am the %s of this website";
-$locale['testimonial_location'] = "and I currently live in %s";
-$locale['testimonial_join'] = "I joined this website on %s";
-$locale['testimonial_web'] = "I also run another website at %s";
-$locale['testimonial_contact'] = "If you need to contact me, you can reach me at %s";
-$locale['testimonial_email'] = "You can also reach me at my e-mail address at %s";
+$locale['blog_1001'] = "Blog - początek";
+$locale['blog_1003'] = "Kategorie";
+$locale['blog_1004'] = "Archiwum bloga";
+$locale['blog_1005'] = "Autorzy";
+$locale['blog_1006'] = "Czytaj więcej";
+$locale['blog_2001'] = "Najnowsze wpisy";
+$locale['blog_2002'] = "Najczęściej komentowane";
+$locale['blog_2003'] = "Najwyżej oceniane";
+$locale['blog_3000'] = "Brak dodanych wpisów";
+$locale['blog_3001'] = "Brak kategorii";
+$locale['blog_3002'] = "Brak wpisów";
+$locale['blog_3003'] = "Brak autorów";
+$locale['testimonial_rank'] = "Na stronie jestem jako %s. ";
+$locale['testimonial_location'] = "Mieszkam w: %s. ";
+$locale['testimonial_join'] = "Na stronie jestem od: %s. ";
+$locale['testimonial_web'] = "Prowadzę stronę internetową pod adresem: %s. ";
+$locale['testimonial_contact'] = "Jeśli chcesz się ze mną skontaktować, możesz łapać mnie na %s. ";
+$locale['testimonial_email'] = "Możesz do mnie pisać na mój adres e-mail: %s";
 
 
