d54a53818fffe1481954c6b1edf881d883512b68
tomeeek64 <mediauser64@hotmail.com>
2016-07-30 21:57:23 +0200

    Update user_phone_mobile.php

--- Polish/locale/Polish/user_fields/user_phone_mobile.php
+++ Polish/locale/Polish/user_fields/user_phone_mobile.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_phone_mobile'] = "Mobile phone";
-$locale['uf_phone_mobile_desc'] = "User mobile phone number";
-$locale['uf_phone_mobile_error'] = "User mobile number can not be left empty.";
+$locale['uf_phone_mobile'] = "Telefon komórkowy";
+$locale['uf_phone_mobile_desc'] = "Numer telefonu komórkowego użytkownika";
+$locale['uf_phone_mobile_error'] = "Pole na numer telefonu komórkowego użytkownika nie może być puste.";
